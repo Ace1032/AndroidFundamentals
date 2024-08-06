93d6d7b52478900c59f5b8ba10c76c072246885b
Imara Dharmakirthi <149819817+Ace1032@users.noreply.github.com>
2024-08-05 19:16:17 -0700

    Delete practice_1 directory

--- practice_1/app/src/main/java/com/example/practice_1/Crime.kt
+++
@@ -1,8 +0,0 @@
-package com.example.practice_1
-
-data class Crime(
-    val id: Int,
-    val title: String,
-    val date: String,
-    val isSolved: Boolean = false
-)
--- practice_1/app/src/main/java/com/example/practice_1/onClickListener.kt
+++
@@ -1,5 +0,0 @@
-package com.example.practice_1
-
-interface onClickListener {
-    fun onClick(crime: Crime)
-}
