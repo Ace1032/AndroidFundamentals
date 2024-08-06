0108855261e598169f260aa1385467388dc2a02f
Imara Dharmakirthi <149819817+Ace1032@users.noreply.github.com>
2024-08-05 18:28:02 -0700

    Delete CriminalIntent directory

--- CriminalIntent/app/src/main/java/com/example/criminalintent/Crime.kt
+++
@@ -1,8 +0,0 @@
-package com.example.criminalintent
-
-import java.util.*
-
-data class Crime(val id: UUID = UUID.randomUUID(),
-                 var title: String = "",
-                 var date: Date = Date(),
-                 var isSolved: Boolean = false){}
